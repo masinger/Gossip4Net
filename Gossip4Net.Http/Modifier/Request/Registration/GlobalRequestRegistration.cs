f2d24bc52d89de5a99f6748acf043b6d6b512c43
Marcel Singer <marcel.singer@live.de>
2022-10-06 09:45:19 +0200

    Allow easy registration manipulations

---
+++ Gossip4Net.Http/Modifier/Request/Registration/GlobalRequestRegistration.cs
@@ -0,0 +1,32 @@
+﻿using Gossip4Net.Http.Builder.Request;
+
+namespace Gossip4Net.Http.Modifier.Request.Registration
+{
+    public class GlobalRequestRegistration : IRequestModifierRegistration
+    {
+        private readonly IList<IHttpRequestModifier> modifiers;
+
+        public GlobalRequestRegistration(IList<IHttpRequestModifier> modifiers)
+        {
+            this.modifiers = modifiers;
+        }
+
+        public GlobalRequestRegistration(params IHttpRequestModifier[] modifiers) 
+            : this(modifiers.ToList()) { }
+
+        public IList<IHttpRequestModifier>? ForMethod(RequestMethodContext methodContext, IEnumerable<Attribute> attributes)
+        {
+            return null;
+        }
+
+        public IList<IHttpRequestModifier>? ForParameter(RequestParameterContext parameterContext, IEnumerable<Attribute> attributes)
+        {
+            return null;
+        }
+
+        public IList<IHttpRequestModifier>? ForType(RequestTypeContext typeContext, IEnumerable<Attribute> attributes)
+        {
+            return modifiers;
+        }
+    }
+}
--- Gossip4Net.Http/Modifier/Response/Registration/ResponseAttributeRegistration.cs
+++ Gossip4Net.Http/Modifier/Response/Registration/ResponseAttributeRegistration.cs
@@ -2,40 +2,8 @@
 
 namespace Gossip4Net.Http.Modifier.Response.Registration
 {
-
-    public interface IResponseAttributeRegistration
+    public interface IResponseModifierRegistration
     {
         public IList<IHttpResponseModifier>? ForMethod(ResponseMethodContext methodContext, IEnumerable<Attribute> attributes);
-    }
-
-    public class ResponseAttributeRegistration<T> : IResponseAttributeRegistration where T : Attribute
-    {
-        private static IList<T> FilterRelevantAttributes(IEnumerable<Attribute> attributes)
-        {
-            return attributes.Select(it => it as T)
-                   .Where(it => it != null)
-                   .Select(it => it!)
-                   .ToList();
-        }
-
-        public virtual IList<IHttpResponseModifier>? ForMethod(ResponseMethodContext methodContext)
-        {
-            return null;
-        }
-
-        public virtual IList<IHttpResponseModifier>? ForMethod(ResponseMethodContext methodContext, IList<T> attributes)
-        {
-            return null;
-        }
-
-        public IList<IHttpResponseModifier>? ForMethod(ResponseMethodContext methodContext, IEnumerable<Attribute> attributes)
-        {
-            IList<T> relevantAttributes = FilterRelevantAttributes(attributes);
-            if (relevantAttributes.Count == 0)
-            {
-                return ForMethod(methodContext);
-            }
-            return ForMethod(methodContext, relevantAttributes);
-        }
     }
 }
