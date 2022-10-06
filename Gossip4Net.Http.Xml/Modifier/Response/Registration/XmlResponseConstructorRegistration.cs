64bf5f306f6ae1c17070720bc6fb62b3ee404b17
Marcel Singer <marcel.singer@live.de>
2022-10-06 15:26:31 +0200

    Add xml deserialization support

---
+++ Gossip4Net.Http.Xml/Modifier/Response/Registration/XmlResponseConstructorRegistration.cs
@@ -0,0 +1,23 @@
+﻿using Gossip4Net.Http.Builder.Response;
+using Gossip4Net.Http.Modifier.Response;
+using Gossip4Net.Http.Modifier.Response.Registration;
+using System.Xml.Serialization;
+
+namespace Gossip4Net.Http.Xml.Modifier.Response.Registration
+{
+    public class XmlResponseConstructorRegistration : IResponseConstructorRegistration
+    {
+        public IList<IResponseConstructor>? ForMethod(ResponseMethodContext responseMethod)
+        {
+            if (responseMethod.IsVoid)
+            {
+                return null;
+            }
+            XmlSerializer xmlSerializer = new XmlSerializer(responseMethod.ProxyReturnType);
+            return new List<IResponseConstructor>
+            {
+                new XmlResponseConstructor(xmlSerializer)
+            };
+        }
+    }
+}
---
+++ Gossip4Net.Http.Xml/Modifier/Response/XmlResponseConstructor.cs
@@ -0,0 +1,32 @@
+﻿using Gossip4Net.Http.Modifier.Response;
+using System.Xml.Serialization;
+
+namespace Gossip4Net.Http.Xml.Modifier.Response
+{
+    internal class XmlResponseConstructor : IResponseConstructor
+    {
+        private readonly XmlSerializer xmlSerializer;
+
+        public XmlResponseConstructor(XmlSerializer xmlSerializer)
+        {
+            this.xmlSerializer = xmlSerializer;
+        }
+
+        private static readonly ISet<string> SupportedMediaTypes = new HashSet<string>()
+        {
+            "application/xml",
+            "text/xml"
+        };
+
+        public async Task<ConstructedResponse> ConstructResponseAsync(HttpResponseMessage response)
+        {
+            string? mediaType = response.Content.Headers.ContentType?.MediaType;
+            if (mediaType == null || !SupportedMediaTypes.Contains(mediaType))
+            {
+                return ConstructedResponse.Empty;
+            }
+            object? responseObject = xmlSerializer.Deserialize(await response.Content.ReadAsStreamAsync());
+            return new ConstructedResponse(responseObject);
+        }
+    }
+}
