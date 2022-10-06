7399cf61ad994406fdeb076f4ec17a113ce4d11a
Marcel Singer <marcel.singer@live.de>
2022-10-06 16:51:32 +0200

    Add XmlRequestBodyModifier

---
+++ Gossip4Net.Http.Xml/GossipHttpRequestBuilderExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Gossip4Net.Http.Xml.Modifier.Request.Registration;
+using Gossip4Net.Http.Xml.Modifier.Response.Registration;
+using System.Xml.Serialization;
+
+namespace Gossip4Net.Http
+{
+    public static class HttpGossipBuilderExtensions
+    {
+
+        public static IHttpGossipBuilder<T> AddXmlBehavior<T>(
+            this IHttpGossipBuilder<T> instance,
+            bool applyByDefault,
+            Func<Type, XmlSerializer> serializerProvider)
+        {
+            return instance
+                .WithRegistrations(r => r
+                    .With(new XmlRequestBodyRegistration(serializerProvider))
+                    .With(new XmlResponseConstructorRegistration(serializerProvider, applyByDefault))
+                );
+        }
+
+        public static IHttpGossipBuilder<T> AddXmlBehavior<T>(
+            this IHttpGossipBuilder<T> instance,
+            bool applyByDefault)
+        {
+            return instance.AddXmlBehavior(
+                applyByDefault,
+                t => new XmlSerializer(t));
+        }
+
+        public static IHttpGossipBuilder<T> AddXmlBehavior<T>(
+            this IHttpGossipBuilder<T> instance)
+        {
+            return instance.AddXmlBehavior(false);
+        }
+
+    }
+}
---
+++ Gossip4Net.Http.Xml/Modifier/Request/XmlRequestBodyModifier.cs
@@ -0,0 +1,40 @@
+﻿using Gossip4Net.Http.Modifier.Request;
+using System.Xml.Serialization;
+
+namespace Gossip4Net.Http.Xml.Modifier.Request
+{
+    internal class XmlRequestBodyModifier : IHttpRequestModifier
+    {
+        private readonly XmlSerializer xmlSerializer;
+        private readonly bool omitEmpty;
+        private readonly int argumentIndex;
+
+        public XmlRequestBodyModifier(
+            XmlSerializer xmlSerializer,
+            bool omitEmpty,
+            int argumentIndex)
+        {
+            this.xmlSerializer = xmlSerializer;
+            this.omitEmpty = omitEmpty;
+            this.argumentIndex = argumentIndex;
+        }
+
+        public Task<HttpRequestMessage> ApplyAsync(HttpRequestMessage requestMessage, object?[] args)
+        {
+            if (requestMessage.Content != null)
+            {
+                return Task.FromResult(requestMessage);
+            }
+
+            object? arg = args[argumentIndex];
+
+            if (arg == null && omitEmpty)
+            {
+                return Task.FromResult(requestMessage);
+            }
+
+            requestMessage.Content = new XmlHttpContent(xmlSerializer, arg);
+            return Task.FromResult(requestMessage);
+        }
+    }
+}
