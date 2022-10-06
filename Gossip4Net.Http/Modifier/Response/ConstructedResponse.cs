94a3039c1d022bcfc91fae68b629a908177ef313
Marcel Singer <marcel.singer@live.de>
2022-10-06 12:51:38 +0200

    Add documentation regarding the internals

--- Gossip4Net.Http/Modifier/Response/ConstructedResponse.cs
+++ Gossip4Net.Http/Modifier/Response/ConstructedResponse.cs
@@ -4,20 +4,7 @@ namespace Gossip4Net.Http.Modifier.Response
     public class ConstructedResponse
     {
         private readonly object? response;
-        public object? Response {
-            get {
-                if (IsEmpty)
-                {
-                    throw new InvalidOperationException("This response is empty.");
-                }
-                return response;
-            }
-        }
-
-        public bool IsEmpty { get; }
-
-        public static ConstructedResponse Empty { get; } = new ConstructedResponse();
-
+        
         public ConstructedResponse(
             object? response
         )
@@ -31,6 +18,22 @@ namespace Gossip4Net.Http.Modifier.Response
             IsEmpty = true;
         }
 
+        public object? Response
+        {
+            get
+            {
+                if (IsEmpty)
+                {
+                    throw new InvalidOperationException("This response is empty.");
+                }
+                return response;
+            }
+        }
+
+        public bool IsEmpty { get; }
+
+        public static ConstructedResponse Empty { get; } = new ConstructedResponse();
+
         public static ConstructedResponse<T> Of<T>(T? value)
         {
             return new ConstructedResponse<T>(value);
@@ -42,6 +45,16 @@ namespace Gossip4Net.Http.Modifier.Response
     public class ConstructedResponse<T> : ConstructedResponse
     {
         private readonly T? response;
+        
+        public ConstructedResponse(T? response) : base(response)
+        {
+            this.response = response;
+        }
+
+        private ConstructedResponse() : base() {
+            response = default(T);
+        }
+
         public new T? Response
         {
             get
@@ -54,16 +67,6 @@ namespace Gossip4Net.Http.Modifier.Response
             }
         }
 
-        public ConstructedResponse(T? response) : base(response)
-        {
-            this.response = response;
-        }
-
-        private ConstructedResponse() : base() {
-            response = default(T);
-        }
-
-       
         public new static ConstructedResponse<T> Empty()
         {
             return new ConstructedResponse<T>();
