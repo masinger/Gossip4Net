6d42dda10836621d41ab10b76f76492f6f000055
Marcel Singer <marcel.singer@live.de>
2022-10-06 16:51:32 +0200

    Add XmlRequestBodyModifier

--- Gossip4Net.Http.Xml.Test/GossipWithXmlTest.cs
+++ Gossip4Net.Http.Xml.Test/GossipWithXmlTest.cs
@@ -1,7 +1,9 @@
 using FluentAssertions;
 using Gossip4Net.Http.Attributes;
 using Gossip4Net.Http.Attributes.Mappings;
+using Gossip4Net.Http.Xml.Modifier.Request.Registration;
 using Gossip4Net.Http.Xml.Modifier.Response.Registration;
+using System.Text.Json;
 using System.Xml.Serialization;
 
 namespace Gossip4Net.Http.Xml.Test
@@ -13,12 +15,20 @@ namespace Gossip4Net.Http.Xml.Test
         public Slideshow() : this(string.Empty, string.Empty) { }
     }
 
+    public record HttpBinResponse(string Data);
+
     [HttpApi("https://httpbin.org")]
     public interface IXmlHttpBinClient {
 
+        [XmlResponse]
         [GetMapping("/xml")]
         Task<Slideshow> GetSlideshow();
+
+        [PostMapping("/post")]
+        Task<HttpBinResponse> PostXml([XmlBody] Slideshow slideshow);
     }
+
+
     
     public class GossipWithXmlTest
     {
@@ -27,17 +37,37 @@ namespace Gossip4Net.Http.Xml.Test
         {
             // Arrange
             IHttpGossipBuilder<IXmlHttpBinClient> builder = new HttpGossipBuilder<IXmlHttpBinClient>()
-                .WithRegistrations(reg => reg.With(new XmlResponseConstructorRegistration()))
+                .WithRegistrations(reg => reg.With(new XmlResponseConstructorRegistration(t => new XmlSerializer(t), false)))
                 .AddDefaultBehavior();
 
             IXmlHttpBinClient client = builder.Build();
-
+            
             // Act
             Slideshow slideshow = await client.GetSlideshow();
 
             // Assert
             slideshow.Title.Should().Be("Sample Slide Show");
             slideshow.Author.Should().Be("Yours Truly");
+        }
+
+        [Fact]
+        public async Task XmlRequestBodyShouldGetSerialized()
+        {
+            // Arrange
+            IHttpGossipBuilder<IXmlHttpBinClient> builder = new HttpGossipBuilder<IXmlHttpBinClient>()
+                .WithRegistrations(reg => reg.With(new XmlRequestBodyRegistration(t => new XmlSerializer(t))))
+                .AddDefaultBehavior(new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                });
+
+            IXmlHttpBinClient client = builder.Build();
+
+            // Act
+            HttpBinResponse response = await client.PostXml(new Slideshow("Test title", "Test author"));
+
+            // Assert
+            response.Data.Should().Contain("title=\"Test title\"").And.Contain("author=\"Test author\"");
         }
     }
 }
